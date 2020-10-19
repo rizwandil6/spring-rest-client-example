71ff5d37e63dfcd3fe79a5d2afce7e211642cf3c
rizwandil6 <rizwan.dil6@gmail.com>
2020-10-19 21:25:54 +0530

    Initial commit

---
+++ src/test/java/adil/springframework/springrestclientexample/SpringRestClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package adil.springframework.springrestclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
